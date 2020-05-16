0b340c897b697d7bf4116bacf1a4a05f964d2f4c
Uche Obianyimuo <uutchei@gmail.com>
2020-05-16 02:06:28 -0500

    final updates

--- PyFinances/Resources/sample.py
+++ PyFinances/Resources/sample.py
@@ -1,66 +1,72 @@
 # Dependencies
+# Dependencies
 import csv
 import os
 
-
 # Files to load and output (Remember to change these)
-
-#file_to_load = "Resources/budget_data.csv"
-#file_to_output = "Resources/budget_analysis.txt"
-
+#file_to_load = os.path.join("Resources", "budget_data.csv")
+#file_to_output = os.path.join("Resources", "budget_analysis.txt")
 
 file_to_load = os.path.join('..', 'Resources', 'budget_data.csv')
 
 file_to_output = os.path.join('..', 'Resources', 'budget_analysis.txt')
 
 
-# Track various revenue parameters
+# Track various financial parameters
 total_months = 0
-prev_revenue = 0
 month_of_change = []
-revenue_change_list = []
+net_change_list = []
 greatest_increase = ["", 0]
 greatest_decrease = ["", 9999999999999999999]
-total_revenue = 0
+total_net = 0
 
 # Read the csv and convert it into a list of dictionaries
-with open(file_to_load) as budget_data:
-    reader = csv.reader(budget_data)
+with open(file_to_load) as budget:
+    reader = csv.reader(budget)
+
+    # Read the header row
+    header = next(reader)
+
+    # Extract first row to avoid appending to net_change_list
+    first_row = next(reader)
+    total_months = total_months + 1
+    total_net = total_net + int(first_row[1])
+    prev_net = int(first_row[1])
 
     for row in reader:
 
         # Track the total
         total_months = total_months + 1
-        total_revenue = total_revenue + int(row[0])
+        total_net = total_net + int(row[1])
 
-        # Track the revenue change
-        revenue_change = int(row[0]) - prev_revenue
-        prev_revenue = int(row[0])
-        revenue_change_list = revenue_change_list + [revenue_change]
-        month_of_change = month_of_change + [row[1]]
+        # Track the net change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list = net_change_list + [net_change]
+        month_of_change = month_of_change + [row[0]]
 
         # Calculate the greatest increase
-        if (revenue_change > greatest_increase[1]):
-            greatest_increase[0] = row[1]
-            greatest_increase[1] = revenue_change
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
 
         # Calculate the greatest decrease
-        if (revenue_change < greatest_decrease[1]):
-            greatest_decrease[0] = row[1]
-            greatest_decrease[1] = revenue_change
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
 
-# Calculate the Average Revenue Change
-revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
+# Calculate the Average Net Change
+net_monthly_avg = sum(net_change_list) / len(net_change_list)
 
 # Generate Output Summary
 output = (
-    f"\nFinancial Analysis\n"
+    f"\nBudget Analysis\n"
     f"----------------------------\n"
     f"Total Months: {total_months}\n"
-    f"Total Revenue: ${total_revenue}\n"
-    f"Average Revenue Change: ${revenue_avg}\n"
-    f"Greatest Increase in Revenue: {greatest_increase[0]} (${greatest_increase[1]})\n"
-    f"Greatest Decrease in Revenue: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+    f"Total: ${total_net}\n"
+    f"Average  Change: ${net_monthly_avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 
 # Print the output (to terminal)
 print(output)
